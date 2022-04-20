a6fdb9ea66cfcdbe22e6d26491375e26b3b23640
BrayanLope5 <74095547+BrayanLope5@users.noreply.github.com>
2022-04-20 14:04:31 -0700

    Added color mode changing buttoon

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import { ChakraProvider, theme, Text, Center } from '@chakra-ui/react';
-import DrawerMenu from './components/Drawer';
+import { ChakraProvider, theme, Text, Center, VStack } from '@chakra-ui/react';
+import DrawerMenu from './components/DrawerMenu';
+import { ColorModeSwitcher } from './components/ColorModeSwitcher';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Center
-        flexDirection='column'
-        h='95vh'
-        bgGradient='linear(to-b, #7928CA, #FF0080)'
-        bgClip='text'
-      >
-        <Text h='20vw' fontSize='20vw'>
-          Riva
-        </Text>
-        <Text h='20vw' fontSize='10vw'>
-          Villa
-        </Text>
+      <Center flexDirection='column' h='100vh' w='100vw'>
+        <ColorModeSwitcher />
+        <VStack bgGradient='linear(to-b, #7928CA, #FF0080)' bgClip='text'>
+          <Text h='20vw' fontSize='20vw'>
+            Riva
+          </Text>
+          <Text h='20vw' fontSize='10vw'>
+            Villa
+          </Text>
+        </VStack>
         <DrawerMenu />
       </Center>
     </ChakraProvider>
---
+++ src/components/ColorModeSwitcher.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  useColorMode,
+  useColorModeValue,
+  IconButton,
+  Box,
+} from '@chakra-ui/react';
+import { FaMoon, FaSun } from 'react-icons/fa';
+
+export const ColorModeSwitcher = (props) => {
+  const { toggleColorMode } = useColorMode();
+  const text = useColorModeValue('dark', 'light');
+  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
+
+  return (
+    <Box w='95vw'>
+      <IconButton
+        size='md'
+        fontSize='lg'
+        aria-label={`Switch to ${text} mode`}
+        variant='ghost'
+        color='current'
+        onClick={toggleColorMode}
+        icon={<SwitchIcon />}
+        {...props}
+      />
+    </Box>
+  );
+};
